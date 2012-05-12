f776626b5814df0b45963eed81fedd45ff873108
Kevin Cantu <me@kevincantu.org>
2012-05-12 16:22:34 -0700

    Now raise an exception when too many words are requested (over 32)

--- password.py
+++ password.py
@@ -1,42 +1,28 @@
 # Kevin Cantu
 # May 2012
 
-import book
-import random
 import scrypt
 
-def kindaRandomOracle(pw0, pw1):
-   return scrypt.hash(pw0, pw1)
-
-def generate(wordList, pw0, pw1, nn=4):
-   avail = len(wordList)
-   entropy = kindaRandomOracle(pw0, pw1)
+def generate(wordList, pass0, pass1, nn=4):
+   entropy = kindaRandomOracle(pass0, pass1)
+   wordCount = len(wordList)
    words = []
 
    bytesPerWord = len(entropy) / nn
 
-#   if bytesPerWord * 8 < avail:
-#      raise Exception('Sorry, running out of bytes to use...')
+   if 2 ** (bytesPerWord * 8) < wordCount:
+      raise Exception('Too many words requested...')
 
    for ii in range(nn):
       key = entropy[ bytesPerWord * (ii) : bytesPerWord * (ii + 1) ]
-      scaledKey = str2int(key) % avail
+      scaledKey = str2int(key) % wordCount
       words.append(wordList[scaledKey])
 
    return ' '.join(words)
    
+def kindaRandomOracle(pw0, pw1):
+   return scrypt.hash(pw0, pw1)
+
 def str2int(s):
    return int(s.encode('hex'), 16)
-
-# grab some words
-def randomWords(book, nn):
-   blen = len(book)
-   words = []
-
-   for ii in range(nn):
-      rnd = random.randrange(0, blen)
-      words.append(book[rnd])
-
-   return words
-
 
