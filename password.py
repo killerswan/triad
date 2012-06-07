b9dad8b6511eab9c3139b3d247e92499364b29fb
Kevin Cantu <me@kevincantu.org>
2012-06-07 00:37:34 -0700

    Add simpler string->int hexlifying

--- password.py
+++ password.py
@@ -1,29 +1,30 @@
 # Kevin Cantu
 # May 2012
 
+import binascii
 import scrypt
 
-def generate(wordList, pass0, pass1, nn=4):
-   entropy = kindaRandomOracle(pass0, pass1)
-   wordCount = len(wordList)
+def generate(vocab, pass0, pass1, nn=4):
+   vocabSize = len(vocab)
    words = []
 
+   entropy = kindaRandomOracle(pass0, pass1)
    bytesPerWord = len(entropy) / nn
+   wordSize = 2 ** (bytesPerWord * 8)
 
-   print "bytes/word: " + str(bytesPerWord)
-
-   wordRange = 2 ** (bytesPerWord * 8)
-
-   if wordRange < wordCount:
+   # if we would lose too much entropy
+   if wordSize < vocabSize:
       raise Exception('Too many words requested...')
 
    for ii in range(nn):
+      # we lose entropy here be dropping words off the end
       key = entropy[ bytesPerWord * (ii) : bytesPerWord * (ii + 1) ]
-      scaledKey = str2int(key) * wordCount / wordRange
-      words.append(wordList[scaledKey])
 
-      print "range is from " + str(bytesPerWord * ii) + " to " + str(bytesPerWord * (ii + 1))
-      print scaledKey
+      # we lose more entropy here by cramming a number into this smaller range
+      scaledKey = str2int(key) * vocabSize / wordSize
+
+      # c'est la vie
+      words.append(vocab[scaledKey])
 
    return ' '.join(words)
    
@@ -31,19 +32,7 @@ def kindaRandomOracle(pw0, pw1):
    val = scrypt.hash(pw0, pw1)
    return val
 
-def str2int(s):
-   '''
-   Note that the way this works is kinda weird...
-      >>> hex(int('1234'.encode('hex'), 16))
-      '0x31323334'
-      >>> hex(int('12345678'.encode('hex'), 16))
-      '0x3132333435363738'
-
-   Use encode to turn each character into a string showing the hex value,
-   then convert each hex string into an integer.
-   '''
-
-   val = int(s.encode('hex'), 16)
-   print hex(val)
+def str2int(ss):
+   val = int(binascii.hexlify(ss), 16)
    return val
 
